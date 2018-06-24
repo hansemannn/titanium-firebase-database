3fd20cb5e8e3c0c28e8525c221faaf0953f253c0
Hans Knöchel <hansemannn@users.noreply.github.com>
2018-06-24 10:28:01 +0200

    Update app.js

--- example/app.js
+++ example/app.js
@@ -1,82 +1,88 @@
 var FirebaseCore = require('firebase.core');
 var FirebaseDatabase = require('firebase.database');
 
-try {
-	FirebaseCore.configure();
-} catch(e) {
-	Ti.API.info(e);
+var plistFile = Ti.Filesystem.getFile(Ti.Filesystem.resourcesDirectory, 'GoogleService-Info.plist');
+
+if (!plistFile.exists()) {
+  alert('Please add your GoogleService-Info.plist file.');
+  return;
 }
 
-var f = Ti.Filesystem.getFile(Ti.Filesystem.resourcesDirectory, 'GoogleService-Info.plist');
-if (f.exists() === false) {
-	alert("Please add your GoogleService-Info.plist file.");
+try {
+  FirebaseCore.configure();
+} catch (e) {
+  Ti.API.error('Error configuring Firebase: ' + e);
 }
 
 var window = Ti.UI.createWindow({
-	title : "Firebase Database",
+  title: 'Firebase Database',
+});
+
+var nav = Ti.UI.iOS.createNavigationWindow({
+  window: window
 });
 
 var tableHeader = Ti.UI.createView({
-	layout : "vertical",
-	height : Ti.UI.SIZE
+  layout: 'vertical',
+  height: Ti.UI.SIZE
 });
 
 var userNameTextField = Ti.UI.createTextField({
-	borderStyle : Ti.UI.INPUT_BORDERSTYLE_ROUNDED,
-	top : "50dp",
-	left : "15dp",
-	right : "15dp",
-	hintText : "Enter unique username"
+  borderStyle: Ti.UI.INPUT_BORDERSTYLE_ROUNDED,
+  top: 50,
+  left: 15,
+  right: 15,
+  hintText: 'Enter unique username'
 });
 
 var userEmailTextField = Ti.UI.createTextField({
-	borderStyle : Ti.UI.INPUT_BORDERSTYLE_ROUNDED,
-	top : "20dp",
-	left : "15dp",
-	right : "15dp",
-	hintText : "Enter email",
+  borderStyle: Ti.UI.INPUT_BORDERSTYLE_ROUNDED,
+  top: 20,
+  left: 15,
+  right: 15,
+  hintText: 'Enter email',
 
 });
 
 var userPasswordTextField = Ti.UI.createTextField({
-	borderStyle : Ti.UI.INPUT_BORDERSTYLE_ROUNDED,
-	top : "20dp",
-	left : "15dp",
-	right : "15dp",
-	hintText : "Enter password",
-	passwordMask : true
+  borderStyle: Ti.UI.INPUT_BORDERSTYLE_ROUNDED,
+  top: 20,
+  left: 15,
+  right: 15,
+  hintText: 'Enter password',
+  passwordMask: true
 });
 
 var registerButton = Ti.UI.createButton({
-	left : "15dp",
-	right : "15dp",
-	top : "20dp",
-	title : "Register"
+  left: 15,
+  right: 15,
+  top: 20,
+  title: 'Register'
 });
 
 var table = Ti.UI.createTableView({
-	headerView : tableHeader,
+  headerView: tableHeader
 });
 
-registerButton.addEventListener("click", function(e) {
+registerButton.addEventListener('click', function(e) {
 
-	if (userNameTextField.value.length == 0 || userEmailTextField.value.length == 0 || userPasswordTextField.value.length == 0) {
-		alert("Please fill all details");
-		return;
-	}
+  if (userNameTextField.value.length == 0 || userEmailTextField.value.length == 0 || userPasswordTextField.value.length == 0) {
+    alert('Please fill all details');
+    return;
+  }
 
-	var fdRef = FirebaseDatabase.getReference().childByAutoId({
-		path : "user" ,
-	});
+  var fdRef = FirebaseDatabase.getReference().childByAutoId({
+    path: 'user',
+  });
 
-	fdRef.setValue({
-		username : userNameTextField.value,
-		email : userEmailTextField.value,
-		password : userPasswordTextField.value,
-		timestamp : FirebaseDatabase.getFirebaseServerTimestamp()
-	}, function(e) {
-		Ti.API.info("Value write callback, Data Snapshot " + JSON.stringify(e));
-	});
+  fdRef.setValue({
+    username: userNameTextField.value,
+    email: userEmailTextField.value,
+    password: userPasswordTextField.value,
+    timestamp: FirebaseDatabase.getFirebaseServerTimestamp()
+  }, function(e) {
+    Ti.API.info('Value written, snapshot: ' + JSON.stringify(e));
+  });
 
 });
 
@@ -88,26 +94,26 @@ tableHeader.add(registerButton);
 window.add(table);
 
 var userRef = FirebaseDatabase.getReference({
-	path : "user",
-	observableEvents : [FirebaseDatabase.DATA_EVENT_TYPE_CHILD_ADDED, FirebaseDatabase.DATA_EVENT_TYPE_VALUE]
+  path: 'user',
+  observableEvents: [FirebaseDatabase.DATA_EVENT_TYPE_CHILD_ADDED, FirebaseDatabase.DATA_EVENT_TYPE_VALUE]
 });
 
-userRef.addEventListener("value", function(e) {
-	Ti.API.info("DATA_EVENT_TYPE_VALUE Data Snapshot " + JSON.stringify(e));
-	var tableData = [];
-	var response = e.value;
-	for (var key in response) {
-		var user_info = response[key];
-		tableData.push({
-			title : user_info.username + " : " + user_info.email,
+userRef.addEventListener('value', function(e) {
+  Ti.API.info('DATA_EVENT_TYPE_VALUE, snapshot: ' + JSON.stringify(e, null, 4));
 
-		});
-	}
-	table.setData(tableData);
+  var tableData = [];
+  var response = e.value;
+
+  for (var key in response) {
+    var user_info = response[key];
+    tableData.push({
+      title: user_info.username + ' : ' + user_info.email,
+    });
+  }
+  table.setData(tableData);
 });
-userRef.addEventListener("add", function(e) {
-	Ti.API.info("DATA_EVENT_TYPE_CHILD_ADDED Data Snapshot " + JSON.stringify(e));
+userRef.addEventListener('add', function(e) {
+  Ti.API.info('DATA_EVENT_TYPE_CHILD_ADDED, snapshot: ' + JSON.stringify(e, null, 4));
 });
 
-window.open();
-
+nav.open();
